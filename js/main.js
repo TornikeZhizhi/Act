37ab2010fa793b36ffff466bc161dd58aba855b0
tornikezhizhi <zhizhiashvilitornike@gmail.com>
2020-04-04 16:48:06 +0400

    file upload bug

--- js/main.js
+++ js/main.js
@@ -73,9 +73,97 @@ $(".see-more").click(function () {
 
 if ($("#myInput1").length > 0) {
 	document.querySelector('.custom-file-input').addEventListener('change',function(e){
-  var fileName = document.getElementById("myInput").files[0].name;
+  var fileName = document.getElementById("myInput1").files[0].name;
   var nextSibling = e.target.nextElementSibling
   nextSibling.innerText = fileName
 })
+
+
 }
+
+ 
+
+
+
+$(document).on('input',"#email", function(e){
+
+
+    var alpabet = [
+         {'@': '@'},
+        {'ქ': 'q'},
+        {'წ': 'w'},
+        {'ე': 'e'},
+        {'რ': 'r'},
+        {'ტ': 't'},
+        {'ყ': 'y'},
+        {'უ': 'u'},
+        {'ი': 'i'},
+        {'ო': 'o'},
+        {'პ': 'p'},
+        {'ა': 'a'},
+        {'ს': 's'},
+        {'დ': 'd'},
+        {'ფ': 'f'},
+        {'გ': 'g'},
+        {'ჰ': 'h'},
+        {'ჯ': 'j'},
+        {'კ': 'k'},
+        {'ლ': 'l'},
+        {'ზ': 'z'},
+        {'ხ': 'x'},
+        {'ც': 'c'},
+        {'ვ': 'v'},
+        {'ბ': 'b'},
+        {'ნ': 'n'},
+        {'მ': 'm'},
+        {'й': 'q'},
+        {'ц': 'w'},
+        {'у': 'e'},
+        {'к': 'r'},
+        {'е': 't'},
+        {'н': 'y'},
+        {'г': 'u'},
+        {'ш': 'i'},
+        {'щ': 'o'},
+        {'з': 'p'},
+        {'ф': 'a'},
+        {'ы': 's'},
+        {'в': 'd'},
+        {'а': 'f'},
+        {'п': 'g'},
+        {'р': 'h'},
+        {'о': 'j'},
+        {'л': 'k'},
+        {'д': 'l'},
+        {'я': 'z'},
+        {'ч': 'x'},
+        {'с': 'c'},
+        {'м': 'v'},
+        {'и': 'b'},
+        {'ть': 'n'},
+        {'ь': 'm'}
+ 
+   ]
+
+
+  
+    var valueBefore =  [...e.target.value.split('')]
+    console.log(e.target.value)
+    valueBefore = valueBefore.map(el => {
+      alpabet.map((lette, i) => {
+        if(lette.hasOwnProperty(el)){
+          el = lette[el]
+        }
+      })
+      return el 
+    })
+    this.value = valueBefore.join('')
+
+   })
+
+
+
+
+
+
 
